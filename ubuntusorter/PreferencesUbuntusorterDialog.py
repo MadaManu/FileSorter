1bc5d21a28c878d02740ae5a004a0858c186c611
MadaManu <madamanu_irl@yahoo.com>
2013-05-23 04:46:54 +0100

    remove folder, dict, small functionality

--- ubuntusorter/PreferencesUbuntusorterDialog.py
+++ ubuntusorter/PreferencesUbuntusorterDialog.py
@@ -25,41 +25,33 @@ class PreferencesUbuntusorterDialog(PreferencesDialog):
         """Set up the preferences dialog"""
         super(PreferencesUbuntusorterDialog, self).finish_initializing(builder)
 
-        folders = self.builder.get_object("folder_list")
-        folders.append(None,"Music")
-        folders.append(None,"Videos")
-        folders.append(None,"Pictures")
+        # map will be replaced by saved settings
+        self.default_map = { 'Music':[".mp3","wav"], 
+                        'Pictures':[".jpg",".png",".bmp",".gif"] }
 
-        thelist = self.builder.get_object("liststore1")
-        thelist.insert(0,("hello0",))
-        thelist.insert(1,("hello1",))
-        thelist.insert(2,("hello2",))
-        thelist.insert(3,("hello3",))
-        thelist.insert(4,("hello4",))
-        thelist.insert(5,("hello5",))
-        thelist.insert(6,("hello6",))
-        thelist.insert(7,("hello7",))
-        thelist.insert(8,("hello8",))
-        thelist.insert(9,("hello9",))
-        thelist.insert(10,("hello10",))
-        thelist.insert(11,("hello11",))
-        thelist.insert(12,("hello12",))
+        # default folders - replaced by saved settings
+        self.default_folders = ["Music", "Pictures", "Videos"]
 
 
-        available = self.builder.get_object("liststore2")
-        available.insert(0,(".avi",))
-        available.insert(1,(".txt",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
-        available.insert(2,(".mp3",))
+        # list of all available extensions - this will be big
+        # and probably fetched from somewhere
+        self.all_extensions = [".mp3",".wav",".jpg",".png",".gif",".bmp",".odt",".sql"]
+
+        # add elements to the dropdown list
+        self.folders = self.builder.get_object("folder_list")
+        for folder in self.default_folders:
+            self.folders.append(None,folder)
+
+        # add default nothing to the extensions
+        # maybe disable the tree 
+        self.thelist = self.builder.get_object("extensions")
+        self.thelist.insert(0,("No Folder",))
+        self.thelist.insert(1,("selected",))
+
+
+        self.available_extensions = self.builder.get_object("available_extensions")
+        for extension in self.all_extensions:
+            self.available_extensions.append((extension,))
 
         # Bind each preference widget to gsettings
         # settings = Gio.Settings("net.launchpad.ubuntusorter")
@@ -67,3 +59,27 @@ class PreferencesUbuntusorterDialog(PreferencesDialog):
         # settings.bind("example", widget, "text", Gio.SettingsBindFlags.DEFAULT)
 
         # Code for other initialization actions should be added here.
+    
+    def on_folder_remove_clicked(self, widget):
+        index = self.folders.get_active()
+        if index>=0:
+            self.default_map.pop(self.default_folders[index],None)  # remove from map
+            self.default_folders.pop(index)                         # remove from the list of folders
+            self.folders.remove(index)                              # remove from the dropdown display
+            print "Folder ",index," Removed" 
+        else:
+            print "Nothing Selected to remove - display some sort of warning box"
+
+    def on_folder_add_clicked(self, widget):
+        # missing text input for the name of the folder - needs redesign or maybe
+        #                                      use some dialog box to input name
+        print "folder added"
+
+    # handler for changing the folder dropdown menu
+    def selected_folder(self, folder_extensions_store):
+
+        folder_extensions_store.clear() # empty the store
+        extensions = self.default_map[self.folders.get_active_text()] # get the list of the extensions
+                                                                      # in the selected folder
+        for extension in extensions: 
+            self.thelist.append((extension,))   # add each extension to the store
